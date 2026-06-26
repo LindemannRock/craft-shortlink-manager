91c4098b664af23030c4b661b81a19fe094ec869
bhlindemann <bh@lindemannrock.com>
2026-06-26 12:24:12 +0400

    refactor(analytics): make isRobot and isMobileApp properties nullable

--- src/records/AnalyticsRecord.php
+++ src/records/AnalyticsRecord.php
@@ -30,8 +30,8 @@ use craft\db\ActiveRecord;
  * @property string|null $osName
  * @property string|null $osVersion
  * @property string|null $clientType
- * @property bool $isRobot
- * @property bool $isMobileApp
+ * @property bool|null $isRobot
+ * @property bool|null $isMobileApp
  * @property string|null $botName
  * @property string|null $botCategory
  * @property string|null $botUrl
