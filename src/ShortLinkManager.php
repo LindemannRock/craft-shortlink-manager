2773fae6991faa561e8889cc854b9337e269f457
bhlindemann <bh@lindemannrock.com>
2026-07-01 10:02:48 +0400

    chore: update copyright year to 2026 in all relevant files

--- src/ShortLinkManager.php
+++ src/ShortLinkManager.php
@@ -5,7 +5,7 @@
  * Advanced shortlink management with QR codes, analytics
  *
  * @link      https://lindemannrock.com
- * @copyright Copyright (c) 2025 LindemannRock
+ * @copyright Copyright (c) 2025-2026 LindemannRock
  */
 
 namespace lindemannrock\shortlinkmanager;
